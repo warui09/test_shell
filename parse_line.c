f1aff1ede8d7e80225a98c9fde2136cd9adbdb8b
Kevin Warui <waruimacharia@gmail.com>
2023-08-06 08:57:26 +0300

    Edit parse_line.changes

--- parse_line.c
+++ parse_line.c
@@ -1,20 +1,32 @@
 #include "main.h"
 
-char **parse_line(char *buffer) {
+/**
+ * parse_line - parse a line of input
+ * @buffer: address of the line of input
+ * 
+ * Return: array of strings containing the parsed input
+ * 
+*/
+
+char **parse_line(char *buffer)
+{
     char *token;
     int token_idx = 0;
     char **tokens = (char **)malloc(TOK_BUFSIZE * sizeof(char *));
 
-    if (!tokens) {
+    if (!tokens)
+    {
         perror("malloc");
         exit(EXIT_FAILURE);
     }
 
     token = strtok(buffer, TOK_DELIM);
 
-    while (token && token_idx < TOK_BUFSIZE - 1) {
+    while (token && token_idx < TOK_BUFSIZE - 1)
+    {
         tokens[token_idx] = (char *)malloc(strlen(token) + 1);
-        if (!tokens[token_idx]) {
+        if (!tokens[token_idx])
+        {
             perror("malloc");
             exit(EXIT_FAILURE);
         }
@@ -24,5 +36,5 @@ char **parse_line(char *buffer) {
     }
 
     tokens[token_idx] = NULL;
-    return tokens;
+    return (tokens);
 }
