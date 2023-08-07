6fa6ff248c0322ebe1a8942768384e6d9593e72b
Arnold Henry <devharnold@gmail.com>
2023-08-07 09:14:50 +0300

    edited-cmd_exec

--- execute_command.c
+++ execute_command.c
@@ -1,5 +1,42 @@
 #include "main.h"
 
+char *search_path(const char *command) {
+        char *path_env = getenv("PATH");
+    char *path, *path_copy, *full_path;
+
+    if(!path_env){
+        fputs("FATAL: Could not retrieve PATH!\n", stderr);
+        return(NULL);
+    }
+
+    path_copy = (char *)malloc(strlen(path_env) +1);
+    if (!path_copy){
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(path_copy, path_env);
+    path = strtok(path_copy, ":");
+    do {
+        full_path = (char *)malloc(strlen(path) + strlen(command) + 2);
+        if (!full_path){
+            perror("malloc");
+            exit(EXIT_FAILURE);
+        }
+
+        sprintf(full_path, "%s/%s", path, command);
+        if (access(full_path, X_OK) == 0){
+            free(path_copy);
+            return (full_path);
+        }
+        free(full_path);
+        path = strtok(NULL, ":");
+    }while (path != NULL);
+
+    free (path_copy);
+    return NULL;
+}
+
 void execute_command(char **command, char **env) {
     char *full_path;
     pid_t pid;
